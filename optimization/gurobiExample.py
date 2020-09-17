72f9576095d05662d29290d2e098e002fe6d7259
Shanmukhi604 <63864161+Shanmukhi604@users.noreply.github.com>
2020-09-17 13:58:18 +0530

    Added the code for objective, other constraints and solving it

--- optimization/gurobiExample.py
+++ optimization/gurobiExample.py
@@ -25,6 +25,7 @@ clinics = list(range(1,i+1))
 time = list(range(1,t+1))
 
 ########################### PARAMETERS ################################
+l = 100000 #large number for consistency constraints
 
 #Transportation cost
 diesel_cost = 14
@@ -135,6 +136,32 @@ Xidt = model.addVars(time,dvs,clinics,vtype=GRB.BINARY, name="Xidt")
 Sijt = model.addVars(time,customers,clinics,vtype=GRB.INTEGER, name="Sijt")
 Wijt = model.addVars(time,customers,clinics,vtype=GRB.INTEGER, name="Wijt")
 
+############################# OBJECTIVE FUNCTION ###########################
+transport_part = gp.quicksum(Kgmt[T-1][M-1][G-1]*Ngmt[T][M][G] for G in gmsd for M in manufacturers for T in time)
+transport_part += gp.quicksum(Ksgt[T-1][G-1][S-1]*Nsgt[T][G][S] for S in svs for G in gmsd for T in time)
+transport_part += gp.quicksum(Krst[T-1][S-1][R-1]*Nrst[T][S][R] for R in rvs for S in svs for T in time)
+transport_part += gp.quicksum(Kdrt[T-1][R-1][D-1]*Ndrt[T][R][D] for D in dvs for R in rvs for T in time)
+transport_part += gp.quicksum(Kidt[T-1][D-1][I-1]*Nidt[T][D][I] for I in clinics for D in dvs for T in time)
+
+inventory_part = gp.quicksum(hgt[T-1][G-1]*Igt[T][G] for G in gmsd for T in time)
+inventory_part += gp.quicksum(hst[T-1][S-1]*Ist[T][S] for S in svs for T in time)
+inventory_part += gp.quicksum(hrt[T-1][R-1]*Irt[T][R] for R in rvs for T in time)
+inventory_part += gp.quicksum(hdt[T-1][D-1]*Idt[T][D] for D in dvs for T in time)
+inventory_part += gp.quicksum(hit[T-1][I-1]*Iit[T][I] for I in clinics for T in time)
+
+shortage_part = gp.quicksum(Pjt[T-1][J-1]*Sijt[T][J][I] for J in customers for I in clinics for T in time)
+
+consumption_part = gp.quicksum(0*Wijt[T][J][I] for J in customers for I in clinics for T in time) #Do we even need this?
+
+ordering_part = gp.quicksum(Cgmt[T-1][M-1][G-1]*Xgmt[T][M][G] for G in gmsd for M in manufacturers for T in time)
+ordering_part += gp.quicksum(Csgt[T-1][G-1][S-1]*Xsgt[T][G][S] for S in svs for G in gmsd for T in time)
+ordering_part += gp.quicksum(Crst[T-1][S-1][R-1]*Xrst[T][S][R] for R in rvs for S in svs for T in time)
+ordering_part += gp.quicksum(Cdrt[T-1][R-1][D-1]*Xdrt[T][R][D] for D in dvs for R in rvs for T in time)
+ordering_part += gp.quicksum(Cidt[T-1][D-1][I-1]*Xidt[T][D][I] for I in clinics for D in dvs for T in time)
+
+model.setObjective(transport_part+inventory_part+shortage_part+consumption_part+ordering_part,GRB.MINIMIZE)
+
+
 ###################################### CONSTRAINTS ################################
 
 #Inventory Balance
@@ -172,3 +199,217 @@ clinic_cap = model.addConstrs((Iit[T][I]<=2000 for I in clinics for T in time),n
 #Production capacity constraints
 production_cap = model.addConstrs((gp.quicksum(Qgmt[T][M][G] for G in gmsd)<= Bmt[T-1][M-1] for M in manufacturers for T in time)
                                 ,name = "production_cap")
+
+#Facility selection constraints
+fac_sg = model.addConstrs((gp.quicksum(Xsgt[T][G][S] for G in gmsd)<=1 for S in svs for T in time),name = "fac_sg")
+fac_rs = model.addConstrs((gp.quicksum(Xrst[T][S][R] for S in svs)<=1 for R in rvs for T in time),name = "fac_rs")
+fac_dr = model.addConstrs((gp.quicksum(Xdrt[T][R][D] for R in rvs)<=1 for D in dvs for T in time),name = "fac_dr")
+fac_id = model.addConstrs((gp.quicksum(Xidt[T][D][I] for D in dvs)<=1 for I in clinics for T in time),name = "fac_id")
+
+#Constraints for consistency of q and X
+cons_1 = model.addConstrs((Xgmt[T][M][G]<=l*Qgmt[T][M][G] for G in gmsd for M in manufacturers for T in time),name = "cons_1")
+cons_2 = model.addConstrs((Qgmt[T][M][G]<=l*Xgmt[T][M][G] for G in gmsd for M in manufacturers for T in time),name = "cons_2")
+cons_3 = model.addConstrs((Xsgt[T][G][S]<=l*Qsgt[T][G][S] for S in svs for G in gmsd for T in time),name = "cons_3")
+cons_4 = model.addConstrs((Qsgt[T][G][S]<=l*Xsgt[T][G][S] for S in svs for G in gmsd for T in time),name = "cons_4")
+cons_5 = model.addConstrs((Xrst[T][S][R]<=l*Qrst[T][S][R] for R in rvs for S in svs for T in time),name = "cons_5")
+cons_6 = model.addConstrs((Qrst[T][S][R]<=l*Xrst[T][S][R] for R in rvs for S in svs for T in time),name = "cons_6")
+cons_7 = model.addConstrs((Xdrt[T][R][D]<=l*Qdrt[T][R][D] for D in dvs for R in rvs for T in time),name = "cons_7")
+cons_8 = model.addConstrs((Qdrt[T][R][D]<=l*Xdrt[T][R][D] for D in dvs for R in rvs for T in time),name = "cons_8")
+cons_9 = model.addConstrs((Xidt[T][D][I]<=l*Qidt[T][D][I] for I in clinics for D in dvs for T in time),name = "cons_9")
+cons_10 = model.addConstrs((Qidt[T][D][I]<=l*Xidt[T][D][I] for I in clinics for D in dvs for T in time),name = "cons_10")
+
+#Number of trucks constraints
+num_trucks_1 = model.addConstrs((Qgmt[T][M][G]/cap_veh_gm<=Ngmt[T][M][G] for G in gmsd for M in manufacturers for T in time),name = "num_trucks_1")
+num_trucks_2 = model.addConstrs((Ngmt[T][M][G]-Qgmt[T][M][G]/cap_veh_gm<=((cap_veh_gm-1)/cap_veh_gm) for G in gmsd for M in manufacturers for T in time),name = "num_trucks_2")
+num_trucks_3 = model.addConstrs((Qsgt[T][G][S]/cap_veh_sg<=Nsgt[T][G][S] for S in svs for G in gmsd for T in time),name = "num_trucks_3")
+num_trucks_4 = model.addConstrs((Nsgt[T][G][S]-Qsgt[T][G][S]/cap_veh_sg<=((cap_veh_sg-1)/cap_veh_sg) for S in svs for G in gmsd for T in time),name = "num_trucks_4")
+num_trucks_5 = model.addConstrs((Qrst[T][S][R]/cap_veh_rs<=Nrst[T][S][R] for R in rvs for S in svs for T in time),name = "num_trucks_5")
+num_trucks_6 = model.addConstrs((Nrst[T][S][R]-Qrst[T][S][R]/cap_veh_rs<=((cap_veh_rs-1)/cap_veh_rs) for R in rvs for S in svs for T in time),name = "num_trucks_6")
+num_trucks_7 = model.addConstrs((Qdrt[T][R][D]/cap_veh_dr<=Ndrt[T][R][D] for D in dvs for R in rvs for T in time),name = "num_trucks_7")
+num_trucks_8 = model.addConstrs((Ndrt[T][R][D]-Qdrt[T][R][D]/cap_veh_dr<=((cap_veh_dr-1)/cap_veh_dr) for D in dvs for R in rvs for T in time),name = "num_trucks_8")
+num_trucks_9 = model.addConstrs((Qidt[T][D][I]/cap_veh_id<=Nidt[T][D][I] for I in clinics for D in dvs for T in time),name = "num_trucks_9")
+num_trucks_10 = model.addConstrs((Nidt[T][D][I]-Qidt[T][D][I]/cap_veh_id<=((cap_veh_id-1)/cap_veh_id) for I in clinics for D in dvs for T in time),name = "num_trucks_10")
+
+#################################Solving the problem##########################
+model.optimize()
+
+for v in model.getVars():
+    print(v.varName,"=", v.x)
+
+###########################Printing the results to excel file################## 
+
+np.random.seed(133)
+Dgm = np.random.normal(1000,250,g*m).reshape(m,g)
+Dsg = np.random.normal(1000,250,s*g).reshape(g,s)
+Drs = np.random.normal(400,75,r*s).reshape(s,r)
+Ddr = np.random.normal(200,25,d*r).reshape(r,d)
+Did = np.random.normal(100,25,d*i).reshape(d,i)
+
+Dgm_name = np.array([["D(g,m)("+str(G)+","+str(M)+")" for G in range(1,g+1)] for M in range(1,m+1)])
+Dsg_name = np.array([["D(s,g)("+str(S)+","+str(G)+")" for S in range(1,s+1)] for G in range(1,g+1)]) 
+Drs_name = np.array([["D(r,s)("+str(R)+","+str(S)+")" for R in range(1,r+1)] for S in range(1,s+1)])
+Ddr_name = np.array([["D(d,r)("+str(D)+","+str(R)+")" for D in range(1,d+1)] for R in range(1,r+1)])
+Did_name = np.array([["D(i,d)("+str(I)+","+str(D)+")" for I in range(1,i+1)] for D in range(1,d+1)])
+
+distances = np.concatenate((Dgm,Dsg,Drs,Ddr,Did),axis=None).tolist()
+D_names = np.concatenate((Dgm_name,Dsg_name,Drs_name,Ddr_name,Did_name),axis=None).tolist()
+
+workbook = xlsxwriter.Workbook('C:/Users/Shanmukhi/Desktop/test.xlsx')
+worksheet = workbook.add_worksheet()
+merge_format = workbook.add_format({
+    'bold': 1,
+    'border': 1,
+    'align': 'center',
+    'valign': 'vcenter'})
+
+worksheet.merge_range('A1:B1', 'Inventory', merge_format)
+worksheet.merge_range('C1:D1', 'Consumption', merge_format)
+worksheet.merge_range('E1:F1', 'Shortage', merge_format)
+worksheet.merge_range('G1:H1', 'Delivery from M to G', merge_format)
+worksheet.merge_range('I1:J1', 'X from M to G', merge_format)
+worksheet.merge_range('K1:L1', 'No of Trucks from M to G', merge_format)
+worksheet.merge_range('M1:N1', 'Distance from M to G', merge_format)
+worksheet.merge_range('O1:P1', 'Delivery from G to S', merge_format)
+worksheet.merge_range('Q1:R1', 'X from G to S', merge_format)
+worksheet.merge_range('S1:T1', 'No of Trucks from G to S', merge_format)
+worksheet.merge_range('U1:V1', 'Distance from G to S', merge_format)
+worksheet.merge_range('W1:X1', 'Delivery from S to R', merge_format)
+worksheet.merge_range('Y1:Z1', 'X from S to R', merge_format)
+worksheet.merge_range('AA1:AB1', 'No of Trucks from S to R', merge_format)
+worksheet.merge_range('AC1:AD1', 'Distance from S to R', merge_format)
+worksheet.merge_range('AE1:AF1', 'Delivery from R to D', merge_format)
+worksheet.merge_range('AG1:AH1', 'X from R to D', merge_format)
+worksheet.merge_range('AI1:AJ1', 'No of Trucks from R to D', merge_format)
+worksheet.merge_range('AK1:AL1', 'Distance from R to D', merge_format)
+worksheet.merge_range('AM1:AN1', 'Delivery from D to I', merge_format)
+worksheet.merge_range('AO1:AP1', 'X from D to I', merge_format)
+worksheet.merge_range('AQ1:AR1', 'No of Trucks from D to I', merge_format)
+worksheet.merge_range('AS1:AT1', 'Distance from D to I', merge_format)
+
+x = 0
+y = 0
+z = 0
+row = 1
+col = 0
+
+#Inventory
+for x in range((g+s+r+d+i)*t):
+    worksheet.write(row, col, names[x])
+    worksheet.write(row, col + 1, round(sol[x]))
+    row += 1
+
+#Qgmt, Xgmt, Ngmt
+row = 1
+for y in range(x+1,x+g*m*t+1):
+    worksheet.write(row, 6, names[y])
+    worksheet.write(row, 7, round(sol[y]))
+    worksheet.write(row, 8, names[y+(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t])
+    worksheet.write(row, 9, round(sol[y+(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t]))
+    worksheet.write(row, 10, names[y+2*(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t])
+    worksheet.write(row, 11, round(sol[y+2*(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t]))
+    row +=1
+
+#Qsgt,Xsgt,Nsgt
+row = 1
+for z in range(y+1,y+s*g*t+1):
+    worksheet.write(row, 14, names[z])
+    worksheet.write(row, 15, round(sol[z]))
+    worksheet.write(row, 16, names[z+(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t])
+    worksheet.write(row, 17, round(sol[z+(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t]))
+    worksheet.write(row, 18, names[z+2*(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t])
+    worksheet.write(row, 19, round(sol[z+2*(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t]))
+    row +=1
+
+#Qrst,Xrst,Nrst
+row = 1
+x = z
+for y in range(x+1,x+r*s*t+1):
+    worksheet.write(row, 22, names[y])
+    worksheet.write(row, 23, round(sol[y]))
+    worksheet.write(row, 24, names[y+(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t])
+    worksheet.write(row, 25, round(sol[y+(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t]))
+    worksheet.write(row, 26, names[y+2*(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t])
+    worksheet.write(row, 27, round(sol[y+2*(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t]))
+    row +=1
+
+#Qdrt,Xdrt,Ndrt
+row = 1
+x = y
+for y in range(x+1,x+r*d*t+1):
+    worksheet.write(row, 30, names[y])
+    worksheet.write(row, 31, round(sol[y]))
+    worksheet.write(row, 32, names[y+(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t])
+    worksheet.write(row, 33, round(sol[y+(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t]))
+    worksheet.write(row, 34, names[y+2*(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t])
+    worksheet.write(row, 35, round(sol[y+2*(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t]))
+    row +=1
+
+#Qidt,Xidt,Nidt
+row = 1
+x = y
+for y in range(x+1,x+i*d*t+1):
+    worksheet.write(row, 38, names[y])
+    worksheet.write(row, 39, round(sol[y]))
+    worksheet.write(row, 40, names[y+(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t])
+    worksheet.write(row, 41, round(sol[y+(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t]))
+    worksheet.write(row, 42, names[y+2*(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t])
+    worksheet.write(row, 43, round(sol[y+2*(g*m+d*r+s*g+r*s+i*d)*t+2*i*j*t]))
+    row +=1
+
+#Shortage
+row = 1
+x = y
+for y in range(x+1,x+i*j*t+1):
+    worksheet.write(row, 4, names[y])
+    worksheet.write(row, 5, round(sol[y]))
+    row +=1
+
+#Consumption
+row = 1
+x = y
+for y in range(x+1,x+i*j*t+1):
+    worksheet.write(row, 2, names[y])
+    worksheet.write(row, 3, round(sol[y]))
+    row +=1
+
+#Distances from M to G
+row = 1
+for y in range(g*m):
+    worksheet.write(row, 12, D_names[y])
+    worksheet.write(row, 13, distances[y])
+    row +=1
+
+#Distances from G to S
+row = 1
+x = y
+for y in range(x+1,x+s*g+1):
+    worksheet.write(row, 20, D_names[y])
+    worksheet.write(row, 21, distances[y])
+    row +=1
+
+#Distances from S to R
+row = 1
+x = y
+for y in range(x+1,x+s*r+1):
+    worksheet.write(row, 28, D_names[y])
+    worksheet.write(row, 29, distances[y])
+    row +=1
+
+#Distances from R to D
+row = 1
+x = y
+for y in range(x+1,x+d*r+1):
+    worksheet.write(row, 36, D_names[y])
+    worksheet.write(row, 37, distances[y])
+    row +=1
+
+#Distances from D to I
+row = 1
+x = y
+for y in range(x+1,x+d*i+1):
+    worksheet.write(row, 44, D_names[y])
+    worksheet.write(row, 45, distances[y])
+    row +=1
+
+workbook.close()
+
