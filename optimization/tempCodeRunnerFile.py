30da19dc567371ed50fa2a4041b400b1e18e00c9
Shanmukhi604 <63864161+Shanmukhi604@users.noreply.github.com>
2020-09-19 15:07:38 +0530

    Made structural changes to the folder

--- optimization/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-or I in clinics for T in time),name = "clinic_cap")
-
-#Production capacity constraints
-production_cap = model.addConstrs((gp.quicksum(Qgmt[T,M,G] for G in gmsd)<= Bmt[T-1][M-1] for M in manufacturers for T in time)
-                                ,name = "production_cap")
-
-#Facility selection constraints
-fac_sg = model.addConstrs((gp.quicksum(Xsgt[T,G,S] for G in gmsd)<=1 for S in svs for T in time),name = "fac_sg")
-fac_rs = model.addConstrs((gp.quicksum
